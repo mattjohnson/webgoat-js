d68df0ba0169e8898dbb0d1d51feeb07f074aad0
Matt Johnson <mjohnson@sonatype.com>
2022-09-26 16:03:38 -0400

    Update test_warnings.sh

--- .muse/test_warnings.sh
+++ .muse/test_warnings.sh
@@ -15,7 +15,7 @@ function run() {
         echo "Args: $*" >> /tmp/x.sh.run.log
         printf "Stdin: " >> /tmp/x.sh.run.log
         #cat <&0 >> /tmp/x.sh.run.log
-        echo '{ "toolNotes" : [
+        echo '{ "tool_notes" : [
                    { "type" : "type during run",
                      "message" : "run message",
                      "file" : "a.c",
@@ -31,40 +31,6 @@ function run() {
               }'
 }
 
-function finalize() {
-        date >> /tmp/x.sh.finalize.log
-        echo "Args: $*" >> /tmp/x.sh.finalize.log
-        printf "Stdin: " >> /tmp/x.sh.finalize.log
-        #cat <&0 >> /tmp/x.sh.finalize.log
-        echo "" >> /tmp/x.sh.finalize.log
-        local pr_branch="v3-tool-run-and-finish-$(date '+%F_%0H-%0M-%0S_%N')"
-        echo "Checking out ${pr_branch} locally" >> /tmp/x.sh.finalize.log
-        git checkout -b $pr_branch &> /dev/null
-        echo "" >> /tmp/x.sh.finalize.log
-        cat <<EOF
-{ "toolNotes" : [
-    { "type" : "type during finalize",
-      "message" : "finalize message",
-      "file" : "a.c",
-      "line" : 3,
-      "noteId" : "noteid"
-    }
-  ]
-, "pullRequest" : {
-      "title": "x.sh PR",
-      "body": "Bug fixes and performance improvements",
-      "target_branch": null,
-      "source_commit": ""
-  }
-, "warnings" : [ 
-    { 
-      "display_message": "display warning during finalize",
-      "detailed_message": "detailed warning during finalize"
-    }
-  ]
-}
-EOF
-}
 
 function tellName() {
         printf "V5Test"
@@ -73,9 +39,6 @@ function tellName() {
 case "$3" in
     run)
         run
-        ;;
-    finalize)
-        finalize
         ;;
     applicable)
         tellApplicable
