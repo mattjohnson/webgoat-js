efb67d9bfed628e725190cf064b84b775a6b4e3e
Matt Johnson <mjohnson@sonatype.com>
2022-09-26 14:46:34 -0400

    testing

--- .muse/test_warnings.sh
+++ .muse/test_warnings.sh
@@ -14,7 +14,7 @@ function run() {
         date >> /tmp/x.sh.run.log
         echo "Args: $*" >> /tmp/x.sh.run.log
         printf "Stdin: " >> /tmp/x.sh.run.log
-        cat <&0 >> /tmp/x.sh.run.log
+        #cat <&0 >> /tmp/x.sh.run.log
         echo '{ "toolNotes" : [
                    { "type" : "type during run",
                      "message" : "run message",
@@ -35,7 +35,7 @@ function finalize() {
         date >> /tmp/x.sh.finalize.log
         echo "Args: $*" >> /tmp/x.sh.finalize.log
         printf "Stdin: " >> /tmp/x.sh.finalize.log
-        cat <&0 >> /tmp/x.sh.finalize.log
+        #cat <&0 >> /tmp/x.sh.finalize.log
         echo "" >> /tmp/x.sh.finalize.log
         local pr_branch="v3-tool-run-and-finish-$(date '+%F_%0H-%0M-%0S_%N')"
         echo "Checking out ${pr_branch} locally" >> /tmp/x.sh.finalize.log
