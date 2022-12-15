759226d36816031b1659fd56a6a03b327fd364d6
Matt Johnson <mjohnson@sonatype.com>
2022-12-15 10:26:53 -0500

    Update setup.sh

--- .muse/setup.sh
+++ .muse/setup.sh
@@ -1,2 +1,10 @@
 #!/bin/bash -
 pip3 install textwrap3
+
+sudo npm i -g eslint@8.29.0 \
+             eslint-plugin-jest \
+             eslint-plugin-babel \
+             eslint-config-airbnb \
+             eslint-config-prettier \
+             eslint-plugin-prettier --save-dev && \
+    sudo npm i -g @typescript-eslint/eslint-plugin@latest --save-dev
